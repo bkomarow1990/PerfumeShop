f3b99434cfb9a54dc07575972fd2ab22a5ee5630
Gnid_ct84 <Gnid_ct84@student.itstep.org>
2022-02-16 15:54:59 +0200

    Add project files.

---
+++ DAL/Data/BrilliantDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class BrilliantDbContext : DbContext
+    {
+        public BrilliantDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
